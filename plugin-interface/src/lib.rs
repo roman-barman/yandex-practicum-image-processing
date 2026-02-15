0e9f2c81f0e918847798eaf30e47158ec1e301ee
Roman Zanko <roman.zanko.vl@gmail.com>
2026-02-15 19:11:41 +0400

    Add `plugin-interface` crate for dynamic library integration

---
+++ plugin-interface/src/lib.rs
@@ -0,0 +1,26 @@
+use libloading::{Library, Symbol};
+use std::ffi::CString;
+
+pub struct PluginInterface<'a> {
+    pub process_image: Symbol<
+        'a,
+        extern "C" fn(width: u32, height: u32, rgb_data: *mut u8, params: *const CString),
+    >,
+}
+
+pub struct Plugin {
+    plugin: Library,
+}
+
+impl Plugin {
+    pub fn new(file_path: &str) -> Result<Self, libloading::Error> {
+        Ok(Plugin {
+            plugin: unsafe { Library::new(file_path) }?,
+        })
+    }
+    pub fn interface(&self) -> Result<PluginInterface<'_>, libloading::Error> {
+        Ok(PluginInterface {
+            process_image: unsafe { self.plugin.get("process_image") }?,
+        })
+    }
+}
