445a04772cc3f1f3cabbc5c21ebf7fb1a8cd6c1e
Roman Zanko <roman.zanko.vl@gmail.com>
2026-02-16 16:38:37 +0400

    Improve error context when loading plugins in `image-processor`

--- image-processor/src/main.rs
+++ image-processor/src/main.rs
@@ -26,7 +26,7 @@ fn main() -> anyhow::Result<()> {
         plugin_path
             .to_str()
             .ok_or(anyhow::anyhow!("plugin path is not valid"))?,
-    )?;
+    ).context("failed to load plugin")?;
     let plugin = plugin
         .interface()
         .context("failed to load plugin interface")?;
